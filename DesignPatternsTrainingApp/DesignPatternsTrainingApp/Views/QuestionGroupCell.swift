1bfb47f0e14928663ada730f0376cb88ae697968
Nikita Shinov <shinov_96@mail.ru>
2022-04-20 16:52:30 +0300

    update of delegate

---
+++ DesignPatternsTrainingApp/DesignPatternsTrainingApp/Views/QuestionGroupCell.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionGroupCell.swift
+//  DesignPatternsTrainingApp
+//
+//  Created by max on 20.04.2022.
+//
+
+import UIKit
+
+class QuestionGroupCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var percentageLabel: UILabel!
+}
